306963709dccec7f39ecd2c34d5ea70398e55e44
Priyatharas <162448996+Priyatharas@users.noreply.github.com>
2026-08-31 15:26:28 +0530

    Time: 330 ms (96.82%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,26 @@
+select visited_on, amount, average_amount
+
+from (
+    select distinct
+        visited_on,
+
+        sum(amount) over (
+            order by visited_on
+            range between interval 6 day preceding and current row
+        ) as amount,
+
+        round(
+            sum(amount) over (
+                order by visited_on
+                range between interval 6 day preceding and current row
+            ) / 7,
+            2
+        ) as average_amount
+
+    from Customer
+) as whole_totals
+
+where datediff(
+    visited_on,
+    (select min(visited_on) from Customer)
+) >= 6;
